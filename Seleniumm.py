358e5815d57b64491ad15967dd80f1092aee8cca
Kikot Serj-Roman <romanovaolesya1@gmail.com>
2021-06-06 13:56:26 +0300

    That was commited

--- Seleniumm.py
+++ Seleniumm.py
@@ -17,4 +17,4 @@ class Wikipedia():
 Theatre=Wikipedia(driver,'https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D0%B0%D1%82%D1%80_(%D0%B0%D1%80%D1%85%D0%B8%D1%82%D0%B5%D0%BA%D1%82%D1%83%D1%80%D0%BD%D0%BE%D0%B5_%D1%81%D0%BE%D0%BE%D1%80%D1%83%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5)#%D0%A3%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE','//h1','//h3[1]')
 Theatre.Title()
 Theatre.H1_elem()
-
+print('Hello World!!!')

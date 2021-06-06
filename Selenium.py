0a7c419037006fbdc99b5bff1b22fd5689d78012
Kikot Serj-Roman <romanovaolesya1@gmail.com>
2021-06-06 13:12:06 +0300

    Selenium 2.0

--- Selenium.py
+++ Selenium.py
@@ -10,11 +10,11 @@ class Wikipedia():
         title_elem=driver.get(self.www)
         title_elem=driver.find_element_by_xpath(self.xpath1)
         print(title_elem.text)
-    def H2_elem(self):
+    def H1_elem(self):
         H2_elem=driver.get(self.www)
         H2_elem=driver.find_element_by_xpath(self.xpath2)
         print(H2_elem.text)
 Theatre=Wikipedia(driver,'https://ru.wikipedia.org/wiki/%D0%A2%D0%B5%D0%B0%D1%82%D1%80_(%D0%B0%D1%80%D1%85%D0%B8%D1%82%D0%B5%D0%BA%D1%82%D1%83%D1%80%D0%BD%D0%BE%D0%B5_%D1%81%D0%BE%D0%BE%D1%80%D1%83%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5)#%D0%A3%D1%81%D1%82%D1%80%D0%BE%D0%B9%D1%81%D1%82%D0%B2%D0%BE','//h1','//h3[1]')
 Theatre.Title()
-Theatre.H2_elem()
+Theatre.H1_elem()
 
